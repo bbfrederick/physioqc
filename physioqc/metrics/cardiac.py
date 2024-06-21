8a550b8fa8b6e698b2c914e1ddf9a1ac53e09ec2
Blaise deB Frederick <blaise.frederick@gmail.com>
2024-06-21 10:27:24 +0900

    Initial work on new metrics

--- physioqc/metrics/cardiac.py
+++ physioqc/metrics/cardiac.py
@@ -1,8 +1,247 @@
 """Denoising metrics for cardio recordings."""
 
 import numpy as np
+from scipy.stats import kurtosis, skew
+from numpy.polynomial import Polynomial
 
 from .. import references
 from ..due import due
 
-pass
+
+def trendgen(thexvals, thefitcoffs, demean):
+    """
+
+    Parameters
+    ----------
+    thexvals: array-like
+        The x-axis value at which to calculate the trend
+    thefitcoffs: array-like
+        The polynomial coefficients used to calculate the trend
+    demean: bool
+        Flag to determine if data should be demeaned
+
+    Returns
+    -------
+
+    """
+    theshape = thefitcoffs.shape
+    order = theshape[0] - 1
+    thepoly = thexvals
+    thefit = 0.0 * thexvals
+    if order > 0:
+        for i in range(1, order + 1):
+            thefit += thefitcoffs[order - i] * thepoly
+            thepoly = np.multiply(thepoly, thexvals)
+    if demean:
+        thefit = thefit + thefitcoffs[order]
+    return thefit
+
+
+def detrend(inputdata, order=1, demean=False):
+    """
+
+    Parameters
+    ----------
+    inputdata: array-like
+        The waveform to be corrected
+    order: int
+        The order of the polynomial
+    demean: bool
+        Flag to determine if data should be demeaned
+
+    Returns
+    -------
+    outputdata: array-like
+        The input waveform with the polynomial baseline removed
+
+    """
+    thetimepoints = np.arange(0.0, len(inputdata), 1.0) - len(inputdata) / 2.0
+    try:
+        thecoffs = Polynomial.fit(thetimepoints, inputdata, order).convert().coef[::-1]
+    except np.lib.polynomial.RankWarning:
+        thecoffs = [0.0, 0.0]
+    thefittc = trendgen(thetimepoints, thecoffs, demean)
+    return inputdata - thefittc
+
+
+
+def calcplethskeqwness(
+    waveform,
+    Fs,
+    S_windowsecs=5.0,
+    detrendorder=8,
+    debug=False,
+):
+    """
+
+    Parameters
+    ----------
+    waveform: array-like
+        The cardiac waveform to be assessed
+    Fs: float
+        The sample rate of the data
+    S_windowsecs: float
+        Skewness window duration in seconds.  Defaults to 5.0 (optimal for discrimination of "good" from "acceptable"
+        and "unfit" according to Elgendi)
+    detrendorder: int
+        Order of detrending polynomial to apply to plethysmogram.
+    debug: boolean
+        Turn on extended output
+
+    Returns
+    -------
+    S_sqi_mean: float
+        The mean value of the quality index over all time
+    S_std_mean: float
+        The standard deviation of the quality index over all time
+    S_waveform: array
+        The quality metric over all timepoints
+
+
+    Calculates the windowed skewness quality metric described in Elgendi, M. in
+    "Optimal Signal Quality Index for Photoplethysmogram Signals". Bioengineering 2016, Vol. 3, Page 21 3, 21 (2016).
+    """
+    # detrend the waveform
+    dt_waveform = detrend(waveform, order=detrendorder, demean=True)
+
+    # calculate S_sqi and K_sqi over a sliding window.  Window size should be an odd number of points.
+    S_windowpts = int(np.round(S_windowsecs * Fs, 0))
+    S_windowpts += 1 - S_windowpts % 2
+    S_waveform = dt_waveform * 0.0
+
+    if debug:
+        print("S_windowsecs, S_windowpts:", S_windowsecs, S_windowpts)
+    for i in range(0, len(dt_waveform)):
+        startpt = np.max([0, i - S_windowpts // 2])
+        endpt = np.min([i + S_windowpts // 2, len(dt_waveform)])
+        S_waveform[i] = skew(dt_waveform[startpt : endpt + 1], nan_policy="omit")
+
+    S_sqi_mean = np.mean(S_waveform)
+    S_sqi_std = np.std(S_waveform)
+
+    return S_sqi_mean, S_sqi_std, S_waveform
+
+def calcplethkurtosis(
+    waveform,
+    Fs,
+    K_windowsecs=60.0,
+    detrendorder=8,
+    debug=False,
+):
+    """
+
+    Parameters
+    ----------
+    waveform: array-like
+        The cardiac waveform to be assessed
+    Fs: float
+        The sample rate of the data
+    K_windowsecs: float
+        Skewness window duration in seconds.  Defaults to 2.0 (after Selveraj)
+    detrendorder: int
+        Order of detrending polynomial to apply to plethysmogram.
+    debug: boolean
+        Turn on extended output
+
+    Returns
+    -------
+    K_sqi_mean: float
+        The mean value of the quality index over all time
+    K_std_mean: float
+        The standard deviation of the quality index over all time
+    K_waveform: array
+        The quality metric over all timepoints
+
+
+    Calculates the windowed kurtosis quality metric described in Elgendi, M. in
+    "Optimal Signal Quality Index for Photoplethysmogram Signals". Bioengineering 2016, Vol. 3, Page 21 3, 21 (2016).
+    """
+    # detrend the waveform
+    dt_waveform = detrend(waveform, order=detrendorder, demean=True)
+
+    # calculate S_sqi and K_sqi over a sliding window.  Window size should be an odd number of points.
+    K_windowpts = int(np.round(K_windowsecs * Fs, 0))
+    K_windowpts += 1 - K_windowpts % 2
+    K_waveform = dt_waveform * 0.0
+
+    if debug:
+        print("K_windowsecs, K_windowpts:", K_windowsecs, K_windowpts)
+    for i in range(0, len(dt_waveform)):
+        startpt = np.max([0, i - K_windowpts // 2])
+        endpt = np.min([i + K_windowpts // 2, len(dt_waveform)])
+        K_waveform[i] = kurtosis(dt_waveform[startpt : endpt + 1], fisher=False)
+
+    K_sqi_mean = np.mean(K_waveform)
+    K_sqi_std = np.std(K_waveform)
+
+    return K_sqi_mean, K_sqi_std, K_waveform
+
+
+def approximateentropy(waveform, m, r):
+    def _maxdist(x_i, x_j):
+        return max([abs(ua - va) for ua, va in zip(x_i, x_j)])
+
+    def _phi(m):
+        x = [[waveform[j] for j in range(i, i + m - 1 + 1)] for i in range(N - m + 1)]
+        C = [len([1 for x_j in x if _maxdist(x_i, x_j) <= r]) / (N - m + 1.0) for x_i in x]
+        return (N - m + 1.0) ** (-1) * sum(np.log(C))
+
+    N = len(waveform)
+
+    return abs(_phi(m + 1) - _phi(m))
+
+def calcplethentropy(
+    waveform,
+    Fs,
+    E_windowsecs=1.0,
+    detrendorder=8,
+    debug=False,
+):
+    """
+
+    Parameters
+    ----------
+    waveform: array-like
+        The cardiac waveform to be assessed
+    Fs: float
+        The sample rate of the data
+    E_windowsecs: float
+        Entropy window duration in seconds.  Defaults to 0.5 (after Selveraj)
+    detrendorder: int
+        Order of detrending polynomial to apply to plethysmogram.
+    debug: boolean
+        Turn on extended output
+
+    Returns
+    -------
+    E_sqi_mean: float
+        The mean value of the quality index over all time
+    E_std_mean: float
+        The standard deviation of the quality index over all time
+    E_waveform: array
+        The quality metric over all timepoints
+
+
+    Calculates the windowed entropy quality metric described in Elgendi, M. in
+    "Optimal Signal Quality Index for Photoplethysmogram Signals". Bioengineering 2016, Vol. 3, Page 21 3, 21 (2016).
+    """
+    # detrend the waveform
+    dt_waveform = detrend(waveform, order=detrendorder, demean=True)
+
+    # calculate S_sqi and K_sqi over a sliding window.  Window size should be an odd number of points.
+    E_windowpts = int(np.round(E_windowsecs * Fs, 0))
+    E_windowpts += 1 - E_windowpts % 2
+    E_waveform = dt_waveform * 0.0
+
+    if debug:
+        print("E_windowsecs, E_windowpts:", E_windowsecs, E_windowpts)
+    for i in range(0, len(dt_waveform)):
+        startpt = np.max([0, i - E_windowpts // 2])
+        endpt = np.min([i + E_windowpts // 2, len(dt_waveform)])
+        r = 0.2 * np.std(dt_waveform[startpt : endpt + 1])
+        E_waveform[i] = approximateentropy(dt_waveform[startpt : endpt + 1], 2, r)
+
+    E_sqi_mean = np.mean(E_waveform)
+    E_sqi_std = np.std(E_waveform)
+
+    return E_sqi_mean, E_sqi_std, E_waveform
--- physioqc/metrics/chest_belt.py
+++ physioqc/metrics/chest_belt.py
@@ -1,8 +1,90 @@
 """Denoising metrics for chest belt recordings."""
 
 import numpy as np
+from .utils import dobpfiltfilt, dolpfiltfilt
+import matplotlib.pyplot as plt
 
 from .. import references
 from ..due import due
 
-pass
+
+def respirationprefilter(rawresp, Fs, lowerpass=0.01, upperpass=2.0, order=1, debug=False):
+    if debug:
+        print(f"respirationprefilter: Fs={Fs} order={order}, lowerpass={lowerpass}, upperpass={upperpass}")
+    return dobpfiltfilt(Fs, lowerpass, upperpass, rawresp, order, debug=debug)
+
+def respenvelopefilter(squarevals, Fs, upperpass=0.1, order=8, debug=False):
+    if debug:
+        print(f"respenvelopefilter: Fs={Fs} order={order}, upperpass={upperpass}")
+    return dolpfiltfilt(Fs, upperpass, squarevals, order, debug=debug)
+
+
+def respiratorysqi(rawresp, Fs, debug=False):
+    """Implementation of Romano's method from A Signal Quality Index for Improving the Estimation of
+    Breath-by-Breath Respiratory Rate During Sport and Exercise,
+    IEEE SENSORS JOURNAL, VOL. 23, NO. 24, 15 DECEMBER 2023"""
+
+    # A. Signal Preprocessing
+    # Apply first order Butterworth bandpass, 0.01-2Hz
+    prefiltered = respirationprefilter(rawresp, Fs, debug=debug)
+    if debug:
+        plt.plot(rawresp)
+        plt.plot(prefiltered)
+        plt.show()
+    if debug:
+        print("prefiltered: ", prefiltered)
+
+    # calculate the derivative
+    derivative = np.gradient(prefiltered, 1.0 / Fs)
+    if debug:
+        plt.plot(prefiltered)
+        plt.plot(derivative)
+        plt.show()
+
+    # normalize the derivative to the range of ~-1 to 1
+    derivmax = np.max(derivative)
+    derivmin = np.min(derivative)
+    derivrange = derivmax - derivmin
+    if debug:
+        print(f"{derivmax=}, {derivmin=}, {derivrange=}")
+    normderiv = 2.0 * (derivative - derivmin) / derivrange - 1.0
+    if debug:
+        plt.plot(normderiv)
+        plt.show()
+
+    # amplitude correct by flattening the envelope function
+    esuperior = 2.0 * respenvelopefilter(np.square(np.where(normderiv > 0.0, normderiv, 0.0)), Fs)
+    esuperior = np.sqrt(np.where(esuperior > 0.0, esuperior, 0.0))
+    einferior = 2.0 * respenvelopefilter(np.square(np.where(normderiv < 0.0, normderiv, 0.0)), Fs)
+    einferior = np.sqrt(np.where(einferior > 0.0, einferior, 0.0))
+    if debug:
+        plt.plot(normderiv)
+        plt.plot(esuperior)
+        plt.plot(-einferior)
+        plt.show()
+    rmsnormderiv = normderiv / (esuperior + einferior)
+    if debug:
+        plt.plot(rmsnormderiv)
+        plt.show()
+
+    # B. Detection of breaths in sliding window
+    seglength = 12.0
+    segsamples = seglength * Fs
+    segstep = 2.0
+    stepsamples = segstep * Fs
+    totaltclength = len(rawresp)
+    numsegs = int(totaltclength // segsamples)
+
+    if totaltclength % segsamples != 0:
+        numsegs += 1
+
+
+    # C. Breaths segmentation
+
+    # D. Similarity Analysis and Exclusion of Unreliable Breaths
+
+    # E. Breath-by-Breath RR Assessment
+
+    return normderiv
+
+
